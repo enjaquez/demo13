1f5a7ad891dc8c6d9b4c9983ef18c7c5c3d7133d
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2022-05-24 12:01:17 -0600

    Update Account Report

--- account_report_extend/__manifest__.py
+++ account_report_extend/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "version": "13.0.1.0.0",
     "category": "Account",
-    "website": "",
+    "website": "https://www.sursoom.mx",
     "author": "Sursoom",
     "license": "AGPL-3",
     "application": False,
